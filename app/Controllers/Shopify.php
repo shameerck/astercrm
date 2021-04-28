4798e9713bb57e3e766bcedbec61bd5548dd3a0f
shameerck <shameerck@gmail.com>
2021-04-28 15:43:50 +0400

    11

--- app/Controllers/Shopify.php
+++ app/Controllers/Shopify.php
@@ -16,7 +16,7 @@ class Shopify extends BaseController
 	{
             $request = \Config\Services::request();
 
-            log_message('info', $request->getJSON());
+            log_message('info', json_decode($request->getJSON()));
             exit;
             $hmac_header = $_GET['hmac'];;
 
