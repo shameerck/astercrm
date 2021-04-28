965538877c9cbe89b8267a0ca8d2ac2e7c25d986
shameerck <shameerck@gmail.com>
2021-04-28 15:56:27 +0400

    11

--- app/Controllers/Shopify.php
+++ app/Controllers/Shopify.php
@@ -16,7 +16,7 @@ class Shopify extends BaseController
 	{
             $request = \Config\Services::request();
 
-            log_message('info', $request->getHeaderLine('HTTP_X_SHOPIFY_HMAC_SHA256'));
+            log_message('info', $request->getGet('HTTP_X_SHOPIFY_HMAC_SHA256'));
             exit;
             $hmac_header = $_GET['hmac'];;
 
