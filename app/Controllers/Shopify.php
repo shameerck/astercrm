7ba2445a1e8e8584cac7453151a9f2a1076d5c4f
shameerck <shameerck@gmail.com>
2021-04-29 16:43:43 +0400

    11

--- app/Controllers/Shopify.php
+++ app/Controllers/Shopify.php
@@ -2,65 +2,48 @@
 
 namespace App\Controllers;
 
+class Shopify extends BaseController {
 
-class Shopify extends BaseController
-{
-    
-    
-    
-    function verify_webhook($data, $hmac_header)
-{
-  $calculated_hmac = base64_encode(hash_hmac('sha256', $data, getenv('SHOPIFY_APP_SECRET'), true));
-  return hash_equals($hmac_header, $calculated_hmac);
-}
+    function verify_webhook($data, $hmac_header) {
+        $calculated_hmac = base64_encode(hash_hmac('sha256', $data, getenv('SHOPIFY_APP_SECRET'), true));
+        return hash_equals($hmac_header, $calculated_hmac);
+    }
 
-	public function customercreated()
-	{
-            $request = \Config\Services::request();
+    public function customercreated() {
+        $request = \Config\Services::request();
 
-            //Read JSON Data
-            $shopify_json = $request->getJSON(true);
-       
-            $hmac_header = $request->getHeaderLine('X-Shopify-Hmac-Sha256');
-            
-/*
- * ob_flush();
-ob_start();
-var_dump($request);
-file_put_contents("dump.txt", ob_get_flush());
-exit;
- * 
- */
-            
-            if($hmac_header!=null)
-            {
-$data = file_get_contents('php://input');
-$verified = $this->verify_webhook($data, $hmac_header);
+        //Read JSON Data
+        $shopify_json = $request->getJSON();
 
-if($verified){
-    $db = \Config\Database::connect();
+        $hmac_header = $request->getHeaderLine('X-Shopify-Hmac-Sha256');
 
-    $data = [
-    'shopify_customer_id'  => $request->getJsonVar('id'),
-    'customer'  => json_encode($shopify_json)
-];
+        /*
+         * ob_flush();
+          ob_start();
+          var_dump($request);
+          file_put_contents("dump.txt", ob_get_flush());
+          exit;
+         * 
+         */
 
-$db->table('customers')->insert($data);
-//    if ($customermodel->insert($data) === false) {
-//        
-//    }
-//    else
-//    {
-//        
-//    }
-            
-}
+        if ($hmac_header != null) {
+            $data = file_get_contents('php://input');
+            $verified = $this->verify_webhook($data, $hmac_header);
 
+            if ($verified) {
+                $db = \Config\Database::connect();
+
+                $data = [
+                    'shopify_customer_id' => $request->getJsonVar('id'),
+                    'customer' => json_encode($shopify_json)
+                ];
+
+                $db->table('customers')->insert($data);
             }
-       else
-       {
-           log_message('error', 'Auth failed.');
-       }
-exit;
-	}
+        } else {
+            log_message('error', 'Auth failed.');
+        }
+        exit;
+    }
+
 }
