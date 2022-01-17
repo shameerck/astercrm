2e9ba0525c2a3843d8b62d703044b4d9a044514c
shameerck <shameerck@gmail.com>
2022-01-17 07:39:44 +0400

    c

--- app/Views/settings.php
+++ app/Views/settings.php
@@ -294,9 +294,9 @@
 
                             $('#dtUnitsList').DataTable({
                                 "aoColumnDefs": [{
-                                        
+                                        "bSortable": true,
                                         "className": "text-left",
-                                        "aTargets": [0, 1, 2, 3, 6, 7, 8, 9, 10]
+                                        "aTargets": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
                                     },
 
                                     {
